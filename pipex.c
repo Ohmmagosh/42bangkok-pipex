71388bfb4a21b0ec3aac29310cc2ee503bea54b3
Phatcharaphol Suanprom <psuanpro@z3t4c1.42bangkok.com>
2022-07-22 18:04:43 +0700

    round2

--- pipex.c
+++ pipex.c
@@ -6,74 +6,68 @@
 /*   By: psuanpro <Marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 20:58:08 by psuanpro          #+#    #+#             */
-/*   Updated: 2022/07/16 00:30:56 by psuanpro         ###   ########.fr       */
+/*   Updated: 2022/07/21 04:43:02 by psuanpro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	chk_path(char	**s)
+char	*ft_join_path(char const *s1, char const *s2)
+{
+	char	*str1;
+	size_t	i;
+
+	if (!s1 || !s2)
+		return (NULL);
+	i = (ft_strlen(s1) + ft_strlen(s2));
+	str1 = (char *)malloc(sizeof(char) * i + 2);
+	if (!str1)
+		return (NULL);
+	while (*s1)
+		*str1++ = *s1++;
+	*str1++ = '/';
+	while (*s2)
+		*str1++ = *s2++;
+	*str1 = '\0';
+	str1 = str1 - i;
+	return ((char *)str1);
+}
+
+void	ft_set_path(char **cmd, char **envp)
 {
 	int	i;
+	char	**path_f;
+	char	**cmd_i;
+	char	**path_j;
 
-	i = 0;
-	while (s[i])
-	{
-		if (ft_strncmp(s[i], "PATH", 4) == 0)
-			break;
-		i++;
-	}
-	return (i);
+	cmd_i = (char **)malloc(sizeof(char *));
+	path_f = (char **)malloc(sizeof(char *));
+	path_j = (char **)malloc(sizeof(char *));
+	cmd_i = ft_split(*cmd, ' ');
+	i = -1;
+	while (ft_strncmp("PATH", envp[++i], 4) != 0)
+		;
+	path_f = ft_split(envp[i] + 5, ':');
+	i = -1;
+	// while (path_f[++i])
+	// 	path_j[i] = ft_strjoin(path_f[i], cmd_i[0]);
+	// i = -1;
+	while (path_f[++i])
+		printf("%s\n", path_f[i]);
 }
 
 int	main(int argc, char **argv, char **envp)
 {
 	(void)argc;
-	(void)**argv;
-	int	i = 0;
-	char *options[3] = {"ls", "-la", NULL};
-	char	**path;
-	char	**pathj;
-	printf("%d\n", i);
+	(void)argv;
+	// int i;
 
-		
-	pathj = (char **)malloc(sizeof(char *));
-
-	path = ft_split(envp[chk_path(envp)]+5,':');
-	// while (path[i]){
-	// 	printf("path = |%s|\n", path[i]);
-	// 	i++;
+	// i = -1;
+	// while(envp[++i]){
+	// 	printf("i = %d\n", i);
+	// 	printf("main = %s\n", envp[i]);
 	// }
-	i = 0;
-	while (path[i])
-	{
-		pathj[i] = ft_strjoin(path[i], "/ls");
-		printf("%s\n", pathj[i]);
-		i++;
-	}
-	while (*pathj)
-	{
-		execve(*pathj, options , envp);
-		// printf("envp %s\n", *envp);
-		pathj++;
-	}
-	return 0;
+	printf("%s\n",ft_join_path("hello", "hello2"));
+	// ft_set_path(&argv[2], envp);
+	return (0);
 }
-
-// > ./pipex file1 cmd1 cmd2 cmd3 ... cmdn file2
-// int	fd;
-// int	id;
-// fd = open(argv[1], O_RDONLY);
-// pipe(fd[2])สร้าง fd ใหม่
-// id = fork();
-// if (id == 0)
-// {
-//		close(fd[0]);
-//		execve();
-// }
-// if (id > 0)
-// {
-//		wait(0);
-//		close(fd[1])
-//		return (fd)
-// }
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: psuanpro <Marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 20:58:38 by psuanpro          #+#    #+#             */
-/*   Updated: 2022/07/14 13:29:21 by psuanpro         ###   ########.fr       */
+/*   Updated: 2022/07/21 03:47:13 by psuanpro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,7 @@ char	**ft_split(char const *s, char c);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 char	*ft_strjoin(char const *s1, char const *s2);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
-
+//test function
+void	ft_set_path(char **cmd, char **envp);
+char	*ft_join_path(char const *s1, char const *s2);
 #endif
